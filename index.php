7d6a334e8f83e81d5fa7d58ff7f9597b2fdb2625
manel <manelgavalda@iesebre.com>
2016-10-13 18:25:56 +0200

    Added Connection querybuilder

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-
+/*
 
 require 'src/core/bootstrap.php';
 require 'src/core/Request.php';
@@ -24,3 +24,27 @@ $routes = require 'routes.php';
 //require $router->direct($uri);
 
 require Manelgavalda\myframework\Router::load('routes.php')->direct(Manelgavalda\myframework\Request::uri());
+*/
+$url = 'http://username:password@hostname:9090/path?arg=value#anchor';
+
+
+
+var_dump(parse_url($url));
+
+var_dump(parse_url($url, PHP_URL_SCHEME));
+
+var_dump(parse_url($url, PHP_URL_USER));
+
+var_dump(parse_url($url, PHP_URL_PASS));
+
+var_dump(parse_url($url, PHP_URL_HOST));
+
+var_dump(parse_url($url, PHP_URL_PORT));
+
+var_dump(parse_url($url, PHP_URL_PATH));
+
+var_dump(parse_url($url, PHP_URL_QUERY));
+
+var_dump(parse_url($url, PHP_URL_FRAGMENT));
+
+?>
