99b59297ced369f07d046d362bb3d5f67299694d
manelgavalda <manelgavalda@iesebre.com>
2016-10-26 21:40:49 +0200

    database

---
+++ src/core/database2/QueryBuilder.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Manelgavalda\myframework\core\database;
+
+use PDO;
+use Manelgavalda\Myframework\core\models\Task;
+
+
+class QueryBuilder {
+
+
+    public $pdo;
+
+    /**
+     * QueryBuilder constructor.
+     * @param $pdo
+     */
+
+    public function __construct(PDO $pdo)
+    {
+        $this->pdo = $pdo;
+    }
+
+
+    function all($table)
+    {
+        $query = $this->pdo->prepare("SELECT * FROM {$table}");
+        $query->execute();
+
+        return $query->fetchAll(PDO::FETCH_CLASS | PDO::FETCH_PROPS_LATE, Task::class);
+    }
+
+    function adduser($name,$table)
+    {
+        $query = $this->pdo->prepare("INSERT INTO {$table} (FirstName) VALUES ('{$name}')");
+        $query->execute();
+
+        return $query->fetchAll(PDO::FETCH_CLASS | PDO::FETCH_PROPS_LATE, Task::class);
+    }
+}
