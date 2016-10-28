e5ec790058f02c3033e5bcc566a603608b17dde7
manelgavalda <manelgavalda@iesebre.com>
2016-10-28 13:52:01 +0200

    v.1

--- src/controllers/routes.php
+++ src/controllers/routes.php
@@ -2,9 +2,9 @@
 
 return [
     ''              =>  __DIR__ .'/tasks.php',
-    'persons'       =>  __DIR__ .'persons.php',
-    'about'         =>  __DIR__ .'about.php',
-    'contact'       =>  __DIR__ .'contact.php',
-    'addperson'     =>  __DIR__ .'addperson.php',
-    'names'         =>  __DIR__ .'names.php',
+    'persons'       =>  __DIR__ .'/persons.php',
+    'about'         =>  __DIR__ .'/about.php',
+    'contact'       =>  __DIR__ .'/contact.php',
+    'addperson'     =>  __DIR__ .'/addperson.php',
+    'names'         =>  __DIR__ .'/names.php',
 ];
