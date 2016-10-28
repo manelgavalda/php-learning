3a6368816eac9f0088398f2a3866933b2bcf458c
manelgavalda <manelgavalda@iesebre.com>
2016-10-28 13:45:18 +0200

    prova

---
+++ src/controllers/routes.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    ''              =>  __DIR__ .'/tasks.php',
+    'persons'       =>  __DIR__ .'persons.php',
+    'about'         =>  __DIR__ .'about.php',
+    'contact'       =>  __DIR__ .'contact.php',
+    'addperson'     =>  __DIR__ .'addperson.php',
+    'names'         =>  __DIR__ .'names.php',
+];
--- tests/index.php
+++ tests/index.php
@@ -4,4 +4,4 @@ use Manelgavalda\Myframework\core\Request;
 use Manelgavalda\Myframework\core\Router;
 
 require __DIR__ .'/../vendor/manelgavalda/php-learning/src/core/bootstrap.php';
-require Router::load(__DIR__ .'/routes.php')->direct(Request::uri());
+require Router::load(__DIR__ .'../vendor/manelgavalda/php-learning/src/controllers/routes.php')->direct(Request::uri());
